339a49037349e287c0f9ddbcc8d98e546173255a
DESKTOP-I8B510D\sela <betsela555@gmail.com>
2023-08-12 16:53:05 +0700

    first project

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text("My app"),),
+        body: Center(
+          child: Text('Learn code weekend'),
+        ),
+      ),
+    );
+  }
+}
+
